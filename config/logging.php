2b4fd64d953da3a4345cb246f2c6789565bea099
Mezu <duzioristo@gmail.com>
2022-04-18 12:49:20 +0100

    heroku 1

--- config/logging.php
+++ config/logging.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'errorlog',
+            'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
